aed647f34a946849c107dbc481854bb960bff791
nacht29 <nacht29.study@gmail.com>
2024-11-20 17:47:31 +0800

    feat: added problem-4: professor

---
+++ cs50p/problem-sets/problem-4/professor/professor.py
@@ -0,0 +1,52 @@
+from random import randint
+from lib import get_int
+
+def main():
+	upper_lim = get_upper_limit()
+	lower_lim = get_lower_limit(upper_lim)
+	game(lower_lim, upper_lim)
+
+def get_upper_limit():
+	while True:
+		level = get_int("Level: ")
+		if level >= 1:
+			break
+
+	upper_lim = 0
+	for i in range(level):
+		upper_lim = upper_lim * 10 + 9
+
+	return (upper_lim)
+
+def get_lower_limit(upper_lim):
+	lower_lim = 1
+
+	while (lower_lim * 10 < upper_lim):
+		lower_lim *= 10
+
+	return (lower_lim)
+
+def game(lower_lim, upper_lim):
+	score = 0
+
+	for i in range(10):
+		x = randint(lower_lim, upper_lim)
+		y = randint(lower_lim, upper_lim)
+		for j in range(3):
+			try:
+				player = int(input(f"{x} + {y} = "))
+			except ValueError:
+				print("EEE")
+				continue
+			else:
+				if (player == x + y):
+					score += 1
+					break
+				elif (j == 2):
+					print(f"{x} + {y} =", x + y)
+				else:
+					print("EEE")
+	print("Score:", score)
+
+if __name__ == '__main__':
+	main()
---
+++ notes/int.py
@@ -0,0 +1,13 @@
+########################
+#float and int division#
+########################
+
+# float division /=
+x = 10
+x /= 3
+print(x) # 3.3333
+
+# integer division //=
+x = 10
+x //= 3
+print(x) # 3.0
