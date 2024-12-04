1195c1181f097f869b56389dcf0617cfbb365603
nacht29 <nacht29.study@gmail.com>
2024-12-04 15:33:49 +0800

    feat: updated notes on functions

--- notes/func.py
+++ notes/func.py
@@ -9,3 +9,35 @@ print(get_str())
 
 def get_str():
 	return "hello world"
+# ============================================================= #
+
+# how to state param datatype
+def prt(msg : str):
+	print(msg)
+
+# show function return value:
+
+# multiple return value
+from typing import Tuple
+
+def rt() -> Tuple[int, str]:
+	return 1, "hello"
+
+def ab() -> Tuple[int, str]:
+	a: int = 1
+	b: str = "hello"
+	return a, b
+
+from typing import Union
+
+def process(value: int) -> Union[int, str]:
+	if value > 0:
+		return value * 2  # Returns an integer
+	else:
+		return "Negative value"  # Returns a string
+
+
+# one return value
+def add(a: int, b: int) -> int:
+	return a + b
+
---
+++ test.py
@@ -0,0 +1,12 @@
+def prt(msg : str):
+	print(msg)
+
+prt("hi")
+
+def rt():
+	return 1, "hello"
+
+def ab():
+	a = 1
+	b = "hello"
+	return a, b
